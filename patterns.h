8ae290cb2d2eca9034720129db92600d80f4b778
Artem Yudin <arrannis1800@gmail.com>
2024-03-21 13:46:06 +0400

    bugfix: fix setting points on edge cases

--- patterns.h
+++ patterns.h
@@ -2,16 +2,24 @@
 #define PATTERNS_H
 #include "structs.h"
 
+void set_dot(World &world, size_t x, size_t y)
+{
+	if(x < world.Width && y < world.Height)
+	{
+		world.level[x + y*world.Width] = true;
+	}
+}
+
 void set_glider(World &world, size_t x, size_t y, EDirecton direction)
 {
 	int x_direction = (direction & ( 1 << 0 )) >> 0 ? 1 : -1;
 	int y_direction = (direction & ( 1 << 1 )) >> 1 ? 1 : -1;
 
-	world.level[(x+1*x_direction) + (y+0*y_direction)*world.Width] = true;
-	world.level[(x+2*x_direction) + (y+1*y_direction)*world.Width] = true;
-	world.level[(x+0*x_direction) + (y+2*y_direction)*world.Width] = true;
-	world.level[(x+1*x_direction) + (y+2*y_direction)*world.Width] = true;
-	world.level[(x+2*x_direction) + (y+2*y_direction)*world.Width] = true;
+	set_dot(world, x+1*x_direction, y+0*y_direction);
+	set_dot(world, x+2*x_direction, y+1*y_direction);
+	set_dot(world, x+0*x_direction, y+2*y_direction);
+	set_dot(world, x+1*x_direction, y+2*y_direction);
+	set_dot(world, x+2*x_direction, y+2*y_direction);
 }
 
 void set_aircraft_carrier(World &world, size_t x, size_t y, EDirecton direction)
@@ -19,12 +27,12 @@ void set_aircraft_carrier(World &world, size_t x, size_t y, EDirecton direction)
 	int x_direction = (direction & ( 1 << 0 )) >> 0 ? 1 : -1;
 	int y_direction = (direction & ( 1 << 1 )) >> 1 ? 1 : -1;
 
-	world.level[(x+0*x_direction) + (y+0*y_direction)*world.Width] = true;
-	world.level[(x+1*x_direction) + (y+0*y_direction)*world.Width] = true;
-	world.level[(x+0*x_direction) + (y+1*y_direction)*world.Width] = true;
-	world.level[(x+3*x_direction) + (y+1*y_direction)*world.Width] = true;
-	world.level[(x+3*x_direction) + (y+2*y_direction)*world.Width] = true;
-	world.level[(x+2*x_direction) + (y+2*y_direction)*world.Width] = true;
+	set_dot(world, x+0*x_direction, y+0*y_direction);
+	set_dot(world, x+1*x_direction, y+0*y_direction);
+	set_dot(world, x+0*x_direction, y+1*y_direction);
+	set_dot(world, x+3*x_direction, y+1*y_direction);
+	set_dot(world, x+3*x_direction, y+2*y_direction);
+	set_dot(world, x+2*x_direction, y+2*y_direction);
 }
 
 void set_line3(World &world, size_t x, size_t y, Eposition position) 
@@ -32,9 +40,9 @@ void set_line3(World &world, size_t x, size_t y, Eposition position)
 	int h_position = (position & ( 1 << 0 )) >> 0 ? 1 : 0;
 	int v_position = (position & ( 1 << 1 )) >> 1 ? 1 : 0;
 
-	world.level[(x+0*h_position) + (y+0*v_position)*world.Width] = true;
-	world.level[(x+1*h_position) + (y+1*v_position)*world.Width] = true;
-	world.level[(x+2*h_position) + (y+2*v_position)*world.Width] = true;
+	set_dot(world, x+0*h_position, y+0*v_position);
+	set_dot(world, x+1*h_position, y+1*v_position);
+	set_dot(world, x+2*h_position, y+2*v_position);
 }
 
 #endif //PATTERNS_H
